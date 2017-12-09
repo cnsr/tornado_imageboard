b5f3a32e285d67148c9f58257fa93c91a7ae0ea9
cnsr <dmytrenkoyar@gmail.com>
2017-12-09 23:35:31 +0200

    added background task scheduler, some other features

--- board.py
+++ board.py
@@ -4,38 +4,51 @@ import tornado.web
 import tornado.ioloop
 import pymongo
 import uimodules
-from datetime import datetime
-
+import datetime
+import ib_settings as _ib
+from tornado import concurrent
 
 from tornado.options import define, options
 define('port', default=8000, help='run on given port', type=int)
 
+executor = concurrent.futures.ThreadPoolExecutor(8)
+
+# should be remade to board handler
 class IndexHandler(tornado.web.RequestHandler):
+
     def get(self):
         db = self.application.database
-        threads = db['posts'].find({'oppost': True}).sort([('_id', -1)]).limit(100)
+        threads = db['posts'].find({'oppost': True}).sort([('lastpost', -1)]).limit(_ib.THREAD_NUMBER)
         self.render('index.html', threads=threads)
+
     def post(self):
         db = self.application.database
-        threads = db['posts'].find({'oppost': True}).sort([('_id', -1)]).limit(100)
+        threads = db['posts'].find({'oppost': True}).sort([('lastpost', -1)]).limit(_ib.THREAD_NUMBER)
         subject = self.get_argument('subject', '')
-        text = self.get_argument('text', 'empty post')
+        text = self.get_argument('text', '')
         count = latest(db) + 1
         oppost = True
         thread = None
         data = makedata(subject, text, count, oppost, thread)
-        db['posts'].insert(data)
+        # doesnt let to post without text
+        if data['text'] != '':
+            db['posts'].insert(data)
         self.render('index.html', threads=threads)
 
 
 class ThreadHandler(tornado.web.RequestHandler):
     thread_count = ''
+
     def get(self, count):
         thread_count = int(count)
         db = self.application.database
         posts = db['posts'].find({'thread': thread_count}).sort([('count', pymongo.ASCENDING)])
         op = db['posts'].find_one({"count": thread_count})
+        if check_thread(db, thread_count, _ib.THREAD_POSTS):
+            op['locked'] = True
+            update_db(db, op['count'], op)
         self.render('posts.html', op=op, posts=posts)
+
     def post(self, thread_count):
         thread_count = int(thread_count)
         db = self.application.database
@@ -45,20 +58,28 @@ class ThreadHandler(tornado.web.RequestHandler):
         oppost = False
         thread = thread_count
         data = makedata(subject, text, count, oppost, thread)
-        db['posts'].insert(data)
         op = db['posts'].find_one({'count': thread_count})
+        if not check_thread(db, thread_count, _ib.THREAD_BUMP):
+            op['lastpost'] = datetime.datetime.utcnow()
+            update_db(db, op['count'], op)
+        if not op['locked']:
+            db['posts'].insert(data)
         posts = db['posts'].find({'thread': thread_count}).sort([('count', pymongo.ASCENDING)])
         self.render('posts.html', op=op, posts=posts)
 
 
+# constructs dictionary to insert into mongodb
 def makedata(subject, text, count, oppost=False, thread=None):
     data = {}
     data['subject'] = subject
     data['text'] = text
     data['count'] = count
-    data['date'] = datetime.utcnow()
+    data['date'] = datetime.datetime.utcnow()
     data['oppost'] = oppost
     data['thread'] = thread
+    if oppost:
+        data['locked'] = False
+        data['lastpost'] = datetime.datetime.utcnow()
     return data
 
 
@@ -70,6 +91,12 @@ def latest(db):
     except Exception as e:
         print(e)
         return 0
+
+
+# checks if number of posts in thread exceeds whatever number you check it against
+def check_thread(db, thread, subj):
+    return db.posts.find({'thread': thread}).count() >= subj
+
 
 class Application(tornado.web.Application):
 
@@ -87,11 +114,37 @@ class Application(tornado.web.Application):
 
         self.con = pymongo.MongoClient('localhost', 27017)
         self.database = self.con['imageboard']
-        try:
-            self.database.create_collection('posts', capped=True, size=10000)
-        except pymongo.errors.CollectionInvalid:
-            pass
         tornado.web.Application.__init__(self, handlers, **settings)
+
+
+# updates one db entry by set parametres
+def update_db(db, count, variables):
+    db.posts.update_one(
+        {'count': count},
+        {
+            '$set': variables
+        }
+    )
+
+
+# deletes the threads that are inactive after there are too much threads
+def schedule_check(app):
+    next_time = datetime.timedelta(0, _ib.CHECK_TIMEOUT)
+    def task():
+        # delete all threads except first N, sorted by bumps
+        col = app.database.posts
+        threads = col.find({'oppost': True}).sort('lastpost', 1)
+        if not threads.count() <= _ib.THREAD_NUMBER:
+            threads = threads.limit(threads.count() - _ib.THREAD_NUMBER)
+            for thread in threads:
+                col.delete_many({'thread': thread['count']})
+                col.remove({'count': thread['count']})
+            print('task finished')
+    def wrapper():
+        executor.submit(task)
+        schedule_check(app)
+    tornado.ioloop.IOLoop.current().add_timeout(next_time, wrapper)
+
 
 
 def main():
@@ -99,6 +152,7 @@ def main():
     application = Application()
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
+    schedule_check(application)
     tornado.ioloop.IOLoop.instance().start()
 
 
---
+++ ib_settings.py
@@ -0,0 +1,8 @@
+# max posts in thread
+THREAD_POSTS = 500
+# thread bump limit
+THREAD_BUMP = 300
+# max threads at same time
+THREAD_NUMBER = 50
+# timeout to function that deletes inactive threads, in second
+CHECK_TIMEOUT = 30
