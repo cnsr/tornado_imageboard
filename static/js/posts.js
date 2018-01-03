fcffa8846bd515f613af62a89d9d974740aea6b8
cnsr <dmytrenkoyar@gmail.com>
2018-01-03 20:48:20 +0200

    played with js and css a bit, tried to make it more mobile-freindly

--- static/js/posts.js
+++ static/js/posts.js
@@ -1,16 +1,41 @@
 $(document).ready(function(){
-	$(".add").draggable({
-	  containment: "window"
-	});
+	if ($(window).width() > 768) {
+		$('.add').addClass('drag');
+		$(".drag").draggable({
+		  containment: "window"
+		});
+	}
 	$('.post-href').click(function() {
 		var number = $(this).attr('href');
 		var addition = '>>';
 		if ($('textarea').val() != '') {addition = '\n>>'};		
 		var new_val = $('textarea').val() + addition + number.substring(1, number.length) + '\n';
 		$('textarea').val(new_val);
+		if (!$('.add').hasClass('drag')) {
+			 //$('.add').get(0).focus();
+			$('html,body').animate({
+				scrollTop: $('.add').offset().top
+			}, 1000);
+			return false;
+		}
 	});
 	$(".post-header").each(function(i) {
 		++i;
     	$(this).find(".post-number").text(i++);
+	});
+	$(window).on('resize', function(){
+		var win = $(this); //this = window
+		if ($(window).width() > 768) {
+			$('.add').addClass('drag');
+			$(".drag").draggable({
+			  containment: "window"
+			});
+		}
+		if (win.width() <= 768) {
+			$('.add').removeClass('drag')
+		}
+		if (win.width() <= 480) {
+			 $(form).hide();
+		}
 	});
 });
