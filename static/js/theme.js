4328dc271ad8c66a7dc82faed968d41bf4e2e494
cnsr <dmytrenkoyar@gmail.com>
2018-02-10 05:29:50 +0200

    added ability to change themes

---
+++ static/js/theme.js
@@ -0,0 +1,30 @@
+var default_theme = 'white.css';
+
+$(document).ready(function(){
+	if (localStorage.theme != '') {
+		get_css(localStorage.theme);
+		$('[value="' + localStorage.theme + '"]').attr('selected', true);
+	} else {
+		get_css(default_theme);
+		$('[value=' + default_theme + ']').attr('selected', true);
+	};
+	$('#themes').change(function () {
+		get_css($(this).val());
+		localStorage.theme = $(this).val().replace("null", default_theme);
+	});
+});
+
+function get_css(file) {
+    "use strict";
+    if ($('#_css')) {
+        $('#_css').remove();
+    }
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.id = '_css';
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/static/' + file;
+    link.media = 'all';
+    $('head').append(link);
+}
