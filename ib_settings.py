1a81fda5c7546e04303927bd809c12e5499dc601
cnsr <dmytrenkoyar@gmail.com>
2018-12-31 13:49:59 +0200

    changed max filesize

--- ib_settings.py
+++ ib_settings.py
@@ -4,6 +4,6 @@ CHECK_TIMEOUT = 15
 # idk how to make this work the proper way tbh
 ADMIN_PASS = 'aaaa1234'
 # maximum filesize on upload
-MAX_FILESIZE = 20971520
+MAX_FILESIZE = 20971520 * 4 # x4 because 4 filefields
 # boards where posting with ban is allowed
 BAN_ALLOWED = ['bans',]
