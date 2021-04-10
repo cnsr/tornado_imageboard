2fcf9dec1d724d759c701af87448ef1dd4c52d0f
cnsr <dmytrenkoyar@gmail.com>
2021-04-10 10:32:57 +0300

    Fixed up admin stats page, fixed popup script not working, fixed some more of admin css

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,13 +1,10 @@
-$(document).ready(function(){
-	$('body').on('click', '#popup-close', function(){
-		$('.popup').fadeOut(400).delay(100).remove();
-	});
-});
+// TODO: animate
+const closePopup = (element) => element.closest('.popup').remove();
 
 function popUp(data) {
 	document.querySelectorAll('.popup').forEach(el => el.remove())
-	var pop = document.createElement('div');
+	let pop = document.createElement('div');
 	pop.classList.add('popup');
-	pop,innerHTML = `<div id="popup-inner">${data}</div><button id="popup-close">X</button>`;
+	pop.innerHTML = `<div id="popup-inner">${data}</div><button id="popup-close" onclick='closePopup(this);'>X</button>`;
 	document.getElementsByTagName('body')[0].appendChild(pop);
 }
